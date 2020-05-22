007c04f6e6a599c429d016778cd15fc98e18978a
Mulishova Ekaterina <mulishova.e@yandex.ru>
2020-05-22 11:15:10 +0300

    update info

--- src/study/studyProject/ClassINFO.java
+++ src/study/studyProject/ClassINFO.java
@@ -1,5 +1,7 @@
 package study.studyProject;
 
+import java.util.List;
+
 public class ClassINFO {
     byte b1_$; // 1b, -128..128
     short s; // 2b, -32 768..32 767
@@ -7,9 +9,13 @@ public class ClassINFO {
     float f; // 4b
 
     int[] arr = {1, 2, 3};
-    // int arr1[] = {1, 2, 3};
-    int[][] arr2 = {{1, 2}, {1, 2}};
+    // int arr1[] = new int[] {1, 2, 3};
+    int[][] arr2 = new int[][] {{1, 2}, {1, 2}};
     // int[] arr3[][];
+
+    Object[] masObject = new Object[10]; // null
+    int[] mas = new int[10]; // 0
+    int[][] masTD = new int[10][];
 
     enum size {SMALL(1), MEDIUM(2) { String getType () { return "B";} }, BIG(3);
         int i;
@@ -28,5 +34,28 @@ public class ClassINFO {
         }
     }
 
+    /* Обертки над примитивными типами ..............................................................................*/
+    Character ch;
+    Integer integer;
+    Byte bt;
+    Short sh;
+    Double db;
+    Float ft;
+    Long lg;
+    Boolean bool;
 
+    Integer str = Integer.valueOf("8");
+    int str1 = Integer.parseInt("9");
+    short str2 = str.shortValue();
+
+    List<Integer> list;
+
+    Object method () {
+        return 1;
+    }
+    /*...............................................................................................................*/
+
+    public static void main(String[] args) {
+        System.out.println(new ClassINFO().str1);
+    }
 }
